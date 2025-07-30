b1f9176da8fe975930e7683726b870ef8e91deb0
Maria Eduarda Batista Henrique <mariaeduardabatt@gmail.com>
2025-07-30 12:31:39 -0300

    header teste

--- home.php
+++ home.php
@@ -43,13 +43,17 @@ if (isset($_POST['modo'])) {
 
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/header.css">
+  
     <link rel="stylesheet" href="./css/home.css">
     <title>Jogo da Memória</title>
+
 </head>
+
 <body>
 
     <?php include_once './php/header.php'; ?>
--- php/header.php
+++ php/header.php
@@ -1,5 +1,12 @@
 <header>
     <span class="titulo-header"><h1><a href="home.php">Jogo da Memória</a></h1></span>
+    <span class = "navbar">
+        <ul>
+            <li><a href="ranking.php">Ranking</a></li>
+            <li><a href="historico.php">Histórico</a></li>
+        
+        </ul>
+    </span>
     <div class="botoes-header">
         <span>Bem-vindo, <?php echo htmlspecialchars($usuario['nome']); ?>!</span>
         <form method="POST">
